20d4ee93a36420f4a80543eeee285b3fb67ee60b
Wadi Chemkhi <wadi3lamda@gmail.com>
2014-01-02 06:07:05 +0100

    adding INSTALL and README files

--- demo/src/main/java/com/diffbot/clients/demo/Demo.java
+++ demo/src/main/java/com/diffbot/clients/demo/Demo.java
@@ -12,7 +12,7 @@ public class Demo {
 
         String testToken="353883355a5c7ff1793b14f81e19c359";
 
-        // Create DiffbotClient instance with the appropriate token
+        //Create DiffbotClient instance with the appropriate token
         DiffbotClient articlesClient = new DiffbotClient(testToken);
 
         /*
@@ -28,6 +28,5 @@ public class Demo {
          */
         BlogPost b= (BlogPost) articlesClient.getArticle(BlogPost.class,"http://www.xconomy.com/san-francisco/2012/07/25/diffbot-is-using-computer-vision-to-reinvent-the-semantic-web/");
         System.out.println(b.toString());
-
     }
 }
